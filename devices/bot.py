198573306482b93dfd154ce2e24e749c257634b2
Inès El Hadri <ines.el.hadri@etu.imt-nord-europe.fr>
2023-12-17 03:15:55 +0100

    message de rappel en mp

--- devices/bot.py
+++ devices/bot.py
@@ -35,9 +35,11 @@ class Bot(discord.Client):
             file.write(json.dumps(self.events))
 
     async def on_ready(self):
+        """when the bot is ready"""
         print(f'Logged in as {self.user} (ID: {self.user.id})')
 
     async def on_message(self, message):
+        """stores the reservation"""
         if message.author == self.user:
             return
 
@@ -45,16 +47,20 @@ class Bot(discord.Client):
             try:
                 assert message.content[8] == ' '  # un espace apres $reserve
                 plage = message.content[9:].split(' ')  # recuperer les plages horaires
-                print(plage)
+
                 heure_debut = time.mktime(time.strptime(plage[0] + ' ' + plage[1], "%d/%m/%Y %H:%M"))
                 heure_fin = time.mktime(time.strptime(plage[0] + ' ' + plage[2], "%d/%m/%Y %H:%M"))
                 assert heure_debut < heure_fin
-                print(heure_debut, heure_fin)
+
             except:
                 await message.channel.send("Le format de réservation est : `$reserve JJ/MM/AAAA hh:mm hh:mm`")
                 return
-            self.events["debuts"][str(message.author.id)] = heure_debut
-            self.events["fins"][str(message.author.id)] = heure_fin
+
+            self.events[message.author.name] = {}
+            self.events[message.author.name]['id'] = message.author.id
+            self.events[message.author.name]['debut'] = heure_debut
+            self.events[message.author.name]['fin'] = heure_fin
+            self.events[message.author.name]['rappel'] = False
             self.save_events()
             await message.channel.send(time.strftime("Succès. Vous avez réservé le %d/%m/%Y de %H:%M",
                                                      time.localtime(heure_debut)) +
@@ -66,13 +72,11 @@ class Bot(discord.Client):
 
     @tasks.loop(seconds=60)
     async def tache_arrplan(self):
-        for i, (k, v) in enumerate(self.events["debuts"].items()):
-            if v - time.time() < 60:
-                channel = self.get_channel(1185600265882173472)
-                await channel.send(f"<@{k}>, tu as réservé ta salle.")
-
-
-
+        for i, (k, v) in enumerate(self.events.items()):
+            user = await self.fetch_user(v['id'])
+            if v['debut'] - time.time() < 600 and not v['rappel']:
+                await user.send(f"<@{v['id']}>, tu as réservé ta salle pour dans 10min")
+                self.events[user]['rappel'] = True
 
     @tache_arrplan.before_loop
     async def before_my_task(self):
